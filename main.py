27c0912ed7a48bfc068103fac6918b1b8fe54105
DANIELS RAIVO IVANOVS 211RMB021 <118028746+DRIYSL@users.noreply.github.com>
2023-03-20 01:09:14 +0200

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,14 @@ def main():
     n,m = map(int, input().split())
     assert 1 <= n <= 10**5,
     assert 1 <= m <= 10**5,
-    data = list(map(int,input().spli()))
+    data = list(map(int,input().split()))
+    assert len(data) == m,
+    assert all(0 <= t <= 10**9 for t in data),
+    
+    result = parallel_processing(n, m, data)
+    
+    for thread, time in result:
+        print(thread, time)
     
 
             
